494a58658d1bad0fdd0f8f4e02bbc6568830c1b5
Den D <denis.derkachzz@gmail.com>
2022-03-16 14:46:41 +0200

    Frames UI in FramesPage, Classical Collage add photo

--- Studio Photo Collage/ViewModels/SidePanelsViewModels/FramesPageViewModel.cs
+++ Studio Photo Collage/ViewModels/SidePanelsViewModels/FramesPageViewModel.cs
@@ -12,6 +12,6 @@ namespace Studio_Photo_Collage.ViewModels.SidePanelsViewModels
             Colors = ColorGenerator.GenerateBrushes();
         }
 
-        public List<SolidColorBrush> Colors { get; set; }
+        public List<SolidColorBrush> Colors { get; }
     }
 }
